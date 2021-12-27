aace9bf9dba8eaabfa7fee07b66697800ef9740a
avzakhman <zaharov4625@gmail.com>
2021-12-27 18:47:53 +0300

    vue.js based portfolio & fixes

--- dist/js/portfolio.js
+++ dist/js/portfolio.js
@@ -1,22 +1,52 @@
-const tags = document.querySelectorAll('.tag-block');
-let tagId = 0;
+// const tags = document.querySelectorAll('.tag-block');
+// let tagId = 0;
 
-function InitTags() {
-  tags.forEach((tag, id)=> {
-    if (id == tagId) {
-      tag.classList.add('active');
-    } else {
-      tag.classList.remove('active');
+// function InitTags() {
+//   tags.forEach((tag, id)=> {
+//     if (id == tagId) {
+//       tag.classList.add('active');
+//     } else {
+//       tag.classList.remove('active');
+//     }
+//   });
+// }
+// InitTags();
+// function SelectTags() {
+//   tags.forEach((tag, id)=> {
+//     tag.addEventListener('click', ()=> {
+//       tagId = id;
+//       InitTags();
+//       showNotFound ();
+//     })
+//   })
+// }
+// SelectTags();
+
+// const projects = document.querySelectorAll('.portfolio-block'),
+//       notFoundMessage = document.querySelector('.portfolio__notfound');
+
+// function showNotFound () {
+//   if ( projects.length == 0 ) {
+//     notFoundMessage.classList.toggle('portfolio__notfound_active');
+//   }
+// }
+
+new Vue ({
+  el: '#portfolio',
+  data: {
+    sortBy: 'all',
+    activeTab: 1
+  },
+  created: function() {
+    this.sortBy = 'all'
+    this.setActive = false
+  },
+  methods: {
+    sortProjects(tag) {
+      this.sortBy = tag;
+    },
+    setActive() {
+      this.setActive = true;
     }
-  });
-}
-InitTags();
-function SelectTags() {
-  tags.forEach((tag, id)=> {
-    tag.addEventListener('click', ()=> {
-      tagId = id;
-      InitTags();
-    })
-  })
-}
-SelectTags();
+  }
+})
