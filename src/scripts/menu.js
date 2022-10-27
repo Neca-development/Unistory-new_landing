5cf1fd1345ecb2d28109ab7272921d51e97eaf13
dynamic1717 <paska-andrej@yandex.ru>
2022-10-27 18:54:52 +0600

    added multi language support

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,8 +1,11 @@
 import "vanilla-hamburger/twirl-burger.js";
+import $ from "jquery";
 
 const burger = document.querySelector("twirl-burger");
 const mobileMenu = document.querySelector(".mobile-menu");
 const allMobileLinks = document.querySelectorAll(".mobile-menu__item");
+
+const languageWindow = $(".header__lngWindow");
 
 burger.addEventListener("pressed-changed", (event) => {
   const pressed = event.detail.value;
@@ -20,4 +23,13 @@ allMobileLinks.forEach((item) => {
   item.addEventListener("click", () => {
     burger.click();
   });
+});
+
+$(".header__lngBtn").on("click", () => {
+  const openedMenuClassName = "header__lngWindow_open";
+  if (languageWindow.hasClass(openedMenuClassName)) {
+    languageWindow.removeClass(openedMenuClassName);
+  } else {
+    languageWindow.addClass(openedMenuClassName);
+  }
 });
