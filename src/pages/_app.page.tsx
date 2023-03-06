9b32410cca005a3915d2d698049a7ae8e5cb8c23
Mironov Ernest <erik1297e@gmail.com>
2023-03-06 18:16:59 +0600

    feat: add google analytic

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,15 +1,29 @@
 import { ThemeContextProvider } from "@shared/lib";
 import "@shared/styles/global.scss";
 import { ThemeProvider } from "next-themes";
-
+import Script from "next/script";
 import type { AppProps } from "next/app";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <ThemeProvider attribute="class">
-    <ThemeContextProvider>
-      <Component {...pageProps} />
-    </ThemeContextProvider>
-  </ThemeProvider>
+	<>
+		<Script
+			strategy="lazyOnload"
+			src="https://www.googletagmanager.com/gtag/js?id=G-Q3N3QQXJV3"
+		/>
+		<Script strategy="lazyOnload" id="google-analytics">
+			{`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag(‘js’, new Date());
+        gtag(‘config’, ‘G-Q3N3QQXJV3’);
+    `}
+		</Script>
+		<ThemeProvider attribute="class">
+			<ThemeContextProvider>
+				<Component {...pageProps} />
+			</ThemeContextProvider>
+		</ThemeProvider>
+	</>
 );
 
 export default MyApp;
