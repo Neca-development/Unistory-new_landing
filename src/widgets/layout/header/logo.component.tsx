04f2d760fd950b3d40007e761ce381faf14cca02
dynamic1717 <paska-andrej@yandex.ru>
2023-03-17 12:00:36 +0600

    Fixed logo without no-ssr

--- src/widgets/layout/header/logo.component.tsx
+++ src/widgets/layout/header/logo.component.tsx
@@ -1,13 +1,23 @@
-import { IconComponent } from "@shared/ui";
-import { useTheme } from "next-themes";
+import { IconComponent } from '@shared/ui'
+import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
 
 export function Logo() {
-	const { theme } = useTheme();
+  const { theme } = useTheme()
+  const [mounted, setMounted] = useState(false)
 
-	return (
-		<IconComponent
-			key={"logo" + theme}
-			name={theme === "dark" ? "logoDark" : "logoLight"}
-		/>
-	);
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
+
+  return (
+    <IconComponent
+      key={'logo' + theme}
+      name={theme === 'dark' ? 'logoDark' : 'logoLight'}
+    />
+  )
 }
