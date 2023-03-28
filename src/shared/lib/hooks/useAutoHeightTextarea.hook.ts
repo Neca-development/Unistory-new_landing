1e1379113e4ccba6e268a8944bf34c7db02aecf0
= <=>
2023-03-28 18:31:00 +0600

    Fix: add constant for value that is subtracted from the height of the textarea

--- src/shared/lib/hooks/useAutoHeightTextarea.hook.ts
+++ src/shared/lib/hooks/useAutoHeightTextarea.hook.ts
@@ -1,12 +1,14 @@
 import { useEffect } from "react"
 
+const normalizeTextareaHeight = 4
+
 export const useAutoHeightTextarea = (ref: HTMLTextAreaElement | null, value?: string) => {
   useEffect(() => {
     if(ref) {
       ref.style.height = "0px"
   
       const scrollValue = ref.scrollHeight
-      ref.style.height = scrollValue - 4 + 'px'
+      ref.style.height = scrollValue - normalizeTextareaHeight + 'px'
     }
   }, [ref, value])
 }
