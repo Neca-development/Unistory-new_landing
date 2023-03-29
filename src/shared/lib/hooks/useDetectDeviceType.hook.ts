94dfa5dd2b2b54c4c134b72e343b72201925b7a7
= <=>
2023-03-29 17:22:50 +0600

    fix: rename useDevice hook

--- src/shared/lib/hooks/useDetectDeviceType.hook.ts
+++ src/shared/lib/hooks/useDetectDeviceType.hook.ts
@@ -1,4 +1,4 @@
-export const useDevice = (width = 640) => {
+export const useDetectDeviceType = (width = 640) => {
   if(typeof window !== 'undefined') {
     return window.matchMedia(`(max-width: ${width}px)`).matches
   }
