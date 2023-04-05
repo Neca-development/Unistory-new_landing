c9c21a5ec69fcbf86d16adc4ca99c36091835a20
= <=>
2023-04-05 18:33:21 +0600

    Feat(adaptive): case page adaptive

--- src/pages/cases/[id]/ui/case-main-info.component.tsx
+++ src/pages/cases/[id]/ui/case-main-info.component.tsx
@@ -9,17 +9,17 @@ function CaseInfoItem({ data }: ICaseInfoItemProps) {
   const {locale} = useRouter()
   
   return (
-    <section className='mb-[7.5rem] last:mb-0'>
+    <section className='mb-[7.5rem] t-xs:mb-16 last:mb-0'>
       <div className='container'>
         <article className='max-w-[52.5rem]'>
-          <h2 className='font-bold text-[2.875rem] whitespace-pre-line'>
+          <h2 className='font-bold text-[2.875rem] whitespace-pre-line t-xs:text-2xl'>
             {data?.title[locale || 'ru']}
           </h2>
           {data?.text?.map((item, idx) => (
-            <div key={idx} className='mt-10'>
-              <h3 className='font-bold text-3xl mb-5'>{item.subtitle ? item.subtitle[locale || 'ru'] : ''}</h3>
+            <div key={idx} className='mt-10 t-xs:mt-4'>
+              <h3 className='font-bold text-3xl mb-5 t-xs:text-xl t-xs:mb-4 t-xs:mt-10'>{item.subtitle ? item.subtitle[locale || 'ru'] : ''}</h3>
               <p
-                className='text-2xl leading-10'
+                className='text-2xl leading-10 t-xs:text-base'
                 dangerouslySetInnerHTML={{ __html: item.text ? item.text[locale || 'ru'] as any : ''}}
               />
             </div>
@@ -29,7 +29,7 @@ function CaseInfoItem({ data }: ICaseInfoItemProps) {
       {data?.banner && (
         <img
           src={typeof data.banner !== 'string' ? data.banner[locale || 'ru'] : data.banner}
-          className='w-full h-auto object-cover mt-[7.5rem]'
+          className='w-full h-auto object-cover mt-[7.5rem] t-xs:mt-10'
         />
       )}
     </section>
@@ -41,7 +41,7 @@ interface ICaseMainInfoProps {
 }
 export function CaseMainInfo({ data }: ICaseMainInfoProps) {
   return (
-    <div className='case-description bg-light-bg-accent dark:bg-dark-bg-accent pt-[7.5rem]'>
+    <div className='case-description bg-light-bg-accent dark:bg-dark-bg-accent pt-[7.5rem] t-xs:pt-16'>
       {data?.map((item, idx) => (
         <CaseInfoItem key={idx} data={item} />
       ))}
--- src/pages/cases/[id]/ui/review.component.tsx
+++ src/pages/cases/[id]/ui/review.component.tsx
@@ -12,16 +12,16 @@ export default function CaseReview({ data }: ICaseReviewProps) {
   const review = data.review
 
   return (
-    <section className='container mt-20 pb-[7.5rem] relative'>
+    <section className='container mt-20 pb-[7.5rem] relative t-xs:mt-16 t-xs:pb-16'>
       <article className='max-w-[52.5rem]'>
-        <p className='text-3xl leading-[3rem] font-medium'>{review?.text[locale || 'ru']}</p>
-        <div className='flex items-center mt-5'>
+        <p className='text-3xl leading-[3rem] font-medium t-xs:text-xl'>{review?.text[locale || 'ru']}</p>
+        <div className='flex items-center mt-5 t-xs:mt-10 t-xs:block'>
           <img
-            className='w-[5.5rem] rounded-full'
+            className='w-[5.5rem] rounded-full t-xs:w-16'
             src={review?.author.photo}
             alt={review?.author.name[locale || 'ru']}
           />
-          <p className='ml-6 text-2xl'>
+          <p className='ml-6 text-2xl t-xs:ml-0 t-xs:text-base t-xs:mt-3'>
             <span className='font-bold'>{review?.author.name[locale || 'ru']}</span>,{' '}
             {review?.author.position[locale || 'ru']}
           </p>
