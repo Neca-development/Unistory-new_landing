38670d0e23a68f18f89515e2f45f8447a801088f
motxxa <92285093+motxxa@users.noreply.github.com>
2023-04-11 18:41:01 +0300

    added reviews

--- src/shared/lib/constants/cases.constants.ts
+++ src/shared/lib/constants/cases.constants.ts
@@ -277,6 +277,22 @@ export const CASES: ICase[] = [
         banner: gasmonstersScheme.src,
       },
     ],
+    review: {
+      author: {
+        name: {
+          en: 'Hiroshi Shimo',
+          ru: 'Хироши Шимо'
+        },
+        position: {
+          en: 'CEO of «Consensus-base Inc.»',
+          ru: 'исполнительный директор «Consensus-base Inc.»'
+        },
+      },
+      text: {
+        en: '«Unistory did a very professional job. They submitted a detailed schedule prior to the estimate and followed it through with no delays. They had a project manager for us the entire time, and when the PM in charge was on vacation, the alternate took care of it. the PM would get back to us as soon as possible. They were also available for technical consultation. They were flexible about ambiguous specifications and cutting edge technology.Other contracts are still ongoing, but we would like to continue to work with them in the future.»',
+        ru: '«Юнистори выполнили свою работу очень профессионально. Они представили подробный график работ согласно подготовленной смете и следовали ему без задержек. Все это время с нами работал проектный менеджер, но как только он ушел в отпуск, команда позаботилась о том, чтобы предоставить  Они также были доступны для технических консультаций. Они проявляли гибкость в отношении неоднозначных спецификаций и передовых технологий. Другие контракты еще не завершены, но мы хотели бы продолжать работать с ними в будущем.»',
+      }
+    },
     team: [
       TEAM.nursultanBegenov,
       TEAM.daniilSemenov,
@@ -646,14 +662,14 @@ export const CASES: ICase[] = [
           ru: 'Богдан Пахоменко'
         },
         position: {
-          en: 'CEO of "BP Networking & Design Inc.',
-          ru: 'исполнительный директор "BP Networking & Design Inc.'
+          en: 'CEO of «BP Networking & Design Inc.»',
+          ru: 'исполнительный директор «BP Networking & Design Inc.»'
         },
         photo: sipsignalClient.src,
       },
       text: {
-        en: '"Good work, friends! Unistory is a well-managed and structured team: weekly updates, quick response times, and quality order fulfillment. I would recommend the Unistory team for any project. We were so pleased with the results that we went straight to our next project. Thank you guys for your hard work."',
-        ru: '«Хорошая работа, друзья! Unistory - это хорошо управляемая и структурированная команда: еженедельные обновления, быстрое реагирование и качественное выполнение заказов. Я бы рекомендовал команду Unistory для любого проекта. Мы были настолько довольны результатами, что сразу же приступили ко следующему проекту. Спасибо вам, ребята, за вашу тяжелую работу.».',
+        en: '«Good work, friends! Unistory is a well-managed and structured team: weekly updates, quick response times, and quality order fulfillment. I would recommend the Unistory team for any project. We were so pleased with the results that we went straight to our next project. Thank you guys for your hard work.»',
+        ru: '«Хорошая работа, друзья! Unistory - это хорошо управляемая и структурированная команда: еженедельные обновления, быстрое реагирование и качественное выполнение заказов. Я бы рекомендовал команду Unistory для любого проекта. Мы были настолько довольны результатами, что сразу же приступили ко следующему проекту. Спасибо вам, ребята, за вашу тяжелую работу.»',
       }
     },
     logo: sipsignalLogo.src,
@@ -886,6 +902,22 @@ export const CASES: ICase[] = [
         banner: hoarderImage3.src
       }
     ],
+    review: {
+      author: {
+        name: {
+          en: 'Connor Walker',
+          ru: 'Коннор Волкер'
+        },
+        position: {
+          en: 'CEO of «HoarderNest»',
+          ru: 'исполнительный директор «HoarderNest»'
+        },
+      },
+      text: {
+        en: '«The skilled team delivered exceptional results and the client is confident that the product launch will be a success. Unistory\'s frequent and clear communication about the project\'s status and roadblocks contributed to a smooth engagement.»' ,
+        ru: '«Это был замечательный опыт от начала до конца! Квалифицированная команда Unistory добилась исключительных результатов, и мы уверены, что запуск продукта будет успешным. Своевременные и четкие оповещения о состоянии проекта и препятствиях по ходу разработки способствовали бесперебойному процессу взаимодействия.»',
+      }
+    },
     team: [
       TEAM.daniilSemenovHead,
       TEAM.ilyaSmirnov,
