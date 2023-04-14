802021730f93d99a77f3c81b6c0cd6bfbada1c30
= <=>
2023-04-14 12:29:04 +0600

    Fix: change stack display

--- src/pages/cases/[id]/ui/stack.component.tsx
+++ src/pages/cases/[id]/ui/stack.component.tsx
@@ -14,18 +14,18 @@ export function Stack({ data }: IStackProps) {
   const langData = useMemo(() => {
     return locale === 'ru' ? SingleCaseRu : SingleCaseEn
   }, [locale])
-  
+  //grid grid-cols-3 gap-10 mt-16 t-xs:mt-6 t-xs:grid-cols-2 t-xs:gap-4
   return (
     <div className='flex bg-light-bg-accent dark:bg-dark-bg-accent pt-20 mt-[7.5rem] t-xs:mt-2 t-xs:pt-10'>
       <div className='container'>
         <h2 className='font-bold text-[2.875rem] t-xs:text-2xl'>{langData.techno}</h2>
-        <div className='grid grid-cols-4 gap-10 mt-16 t-xs:mt-6 t-xs:grid-cols-2 t-xs:gap-4'>
+        <div className='flex justify-center items-center gap-10 mt-16 flex-wrap  t-xs:mt-6 t-xs:gap-4'>
           {data?.map((technology, idx) => (
             <div
               key={idx}
               className={clsx(
-                'flex bg-light-bg dark:bg-dark-bg py-8 px-[1.875rem] flex-col items-center',
-                idx === data?.length - 1 && idx % 2 === 0 && 'col-span-2'
+                'flex bg-light-bg w-[25rem] dark:bg-dark-bg py-8 px-[1.875rem] flex-col items-center',
+                // idx === data?.length - 1 && idx % 2 === 0 && 'col-span-2'
               )}
             >
               <img className='h-[7.125rem] t-xs:h-16' src={technology.icon} alt='' />
