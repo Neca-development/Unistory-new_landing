75374c189cab64fb3199c877c0ea4fdc30beacd1
Maxim <uporoff.maxim@yandex.ru>
2023-06-01 14:46:01 +0600

    fix comments

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,15 +2,12 @@ import { ThemeContextProvider } from "@shared/lib";
 import "@shared/styles/global.scss";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
-import { Suspense } from "react";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
 	return <ThemeProvider attribute="class">
 		<ThemeContextProvider>
-			<Suspense>
-				<Component {...pageProps} />
-				<div id="portal" />
-			</Suspense>
+			<Component {...pageProps} />
+			<div id="portal" />
 		</ThemeContextProvider>
 	</ThemeProvider>
 };
