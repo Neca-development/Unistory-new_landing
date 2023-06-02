532fdd1ed571dfaf3c800177cf48596d37d81d2e
Daniil <boiko.81@list.ru>
2023-06-02 18:03:09 +0300

    fix: add sticky style for bottom-form title only for mobile+ screens

--- src/widgets/connect/connect.component.tsx
+++ src/widgets/connect/connect.component.tsx
@@ -1,28 +1,23 @@
-import React, { useMemo } from 'react'
-import ConnectForm from './connect-form.component'
-import {FormEn, FormRu} from "@shared/i18n"
-import { useRouter } from 'next/router'
+import React, { useMemo } from "react";
+import ConnectForm from "./connect-form.component";
+import { FormEn, FormRu } from "@shared/i18n";
+import { useRouter } from "next/router";
 
 export function Connect() {
-  const {locale}  = useRouter()
+  const { locale } = useRouter();
 
   const data = useMemo(() => {
-    return locale === 'en' ?  FormEn : FormRu
-  }, [locale])
+    return locale === "en" ? FormEn : FormRu;
+  }, [locale]);
 
   return (
-    <section className='bg-dark-surface' id="become-customer">
-      <div className='container pt-16 pb-[5.5rem] grid grid-cols-[1fr_38.75rem] gap-[10rem] text-dark-text-primary t-xs:grid-cols-1 t-xs:gap-0 relative'>
-        <div className="sticky top-40 h-fit">
-          <h2 className='font-bold text-5xl leading-[3.25rem] t-xs:text-2xl'>
-						{data.slogan}
-          </h2>
-          <p className='mt-9 text-2xl leading-10 t-xs:text-base'>
-						{data.description.beforeEmail}{' '}
-            <a
-              href='mailto:contact@unistory.app'
-              className='text-primary-s font-bold'
-            >
+    <section className="bg-dark-surface" id="become-customer">
+      <div className="container pt-16 pb-[5.5rem] grid grid-cols-[1fr_38.75rem] gap-[10rem] text-dark-text-primary t-xs:grid-cols-1 t-xs:gap-0 relative">
+        <div className="md:sticky md:top-40 h-fit">
+          <h2 className="font-bold text-5xl leading-[3.25rem] t-xs:text-2xl">{data.slogan}</h2>
+          <p className="mt-9 text-2xl leading-10 t-xs:text-base">
+            {data.description.beforeEmail}{" "}
+            <a href="mailto:contact@unistory.app" className="text-primary-s font-bold">
               contact@unistory.app
             </a>
             . {data.description.afterEmail}
@@ -32,5 +27,5 @@ export function Connect() {
         <ConnectForm />
       </div>
     </section>
-  )
+  );
 }
