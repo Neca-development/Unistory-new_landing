e740640f55c914ffc4d9748625e0ca8da99e71e1
Daniil <boiko.81@list.ru>
2023-06-06 14:16:17 +0300

    feat: add dynamic favicon (depends on theme)

--- src/shared/meta/meta.tsx
+++ src/shared/meta/meta.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { NextSeo } from "next-seo";
+import { useTheme } from "next-themes";
 
 import { APP_META } from "./meta.config";
 
@@ -13,27 +14,36 @@ type IMetaProperties = {
 const Meta = (props: IMetaProperties) => {
   const router = useRouter();
 
+  const { theme } = useTheme();
+  const faviconPrefix = theme === "dark" ? "-dark" : "";
+
+  console.log(theme);
+
   return (
     <>
       <Head>
         <meta charSet="utf8" key="charset" />
         <meta name="viewport" content="width=device-width,initial-scale=1" key="viewport" />
-        <link rel="apple-touch-icon" href={`${router.basePath}/apple-touch-icon.png`} key="apple" />
+        <link
+          rel="apple-touch-icon"
+          href={`${router.basePath}/apple-touch-icon${faviconPrefix}.png`}
+          key="apple"
+        />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href={`${router.basePath}/favicon-32x32.png`}
+          href={`${router.basePath}/favicon-32x32${faviconPrefix}.png`}
           key="icon32"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href={`${router.basePath}/favicon-16x16.png`}
+          href={`${router.basePath}/favicon-16x16${faviconPrefix}.png`}
           key="icon16"
         />
-        <link rel="icon" href={`${router.basePath}/favicon.ico`} key="favicon" />
+        <link rel="icon" href={`${router.basePath}/favicon${faviconPrefix}.ico`} key="favicon" />
       </Head>
       <NextSeo
         title={props.title}
