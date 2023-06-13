00ceaa0e08ad10c8b9dd1afc3902a7da3f0e23b2
Daniil <boiko.81@list.ru>
2023-06-13 17:14:01 +0300

    feat: update width of logo

--- src/widgets/layout/header/logo.component.tsx
+++ src/widgets/layout/header/logo.component.tsx
@@ -7,53 +7,64 @@ import { useAnimationStore } from "@shared/lib/store";
 import { useRouter } from "next/router";
 
 export function Logo() {
-	const { theme, systemTheme } = useTheme();
-	const {temperatureTheme} = useThemeContext()
-	const _theme = theme === "system" ? systemTheme : theme;
-	const mounted = useMounted()
+  const { theme, systemTheme } = useTheme();
+  const { temperatureTheme } = useThemeContext();
+  const _theme = theme === "system" ? systemTheme : theme;
+  const mounted = useMounted();
 
-	const {asPath} = useRouter()
-	const isHome = asPath === '/'
-	const {shouldAnimate} = useAnimationStore()
+  const { asPath } = useRouter();
+  const isHome = asPath === "/";
+  const { shouldAnimate } = useAnimationStore();
 
-	if (!mounted) {
-		return null;
-	}
+  if (!mounted) {
+    return null;
+  }
 
-	return (
-		<div
-			className={clsx(
-				"absolute top-1/2 left-0 -translate-y-1/2 ml-[0.5rem] lg:ml-[2rem]",
-			isHome && shouldAnimate && 'will-change-[top,left] animate-logo-enter-mobile lg:animate-logo-enter-lg'
-		)}>
-			<div className={clsx(
-				"overflow-hidden flex mt-7",
-				_theme === 'dark' ? 'flex-col-reverse' : 'flex-col',
-				isHome && shouldAnimate && 'animate-logo-overflow-mobile lg:animate-logo-overflow-lg will-change-[transform,width]'
-			)}>
-				<IconComponent
-					key="logoLight"
-					name="logoLight"
-					width="8rem"
-					className={clsx(
-						"duration-300",
-						isHome && shouldAnimate && "animate-main-logo-width-mobile lg:animate-main-logo-width-lg will-change-[width]",
-						_theme !== 'light' && 'opacity-0',
-						temperatureTheme === 'winter' ? "fill-winter" : "fill-summer"
-					)}
-				/>
-				<IconComponent
-					key="logoDark"
-					name="logoDark"
-					width="8rem"
-					className={clsx(
-						"duration-300",
-						isHome && shouldAnimate && "animate-main-logo-width-mobile lg:animate-main-logo-width-lg will-change-[width]",
-						_theme !== 'dark' && 'opacity-0',
-						temperatureTheme === 'winter' ? "fill-winter" : "fill-summer"
-					)}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className={clsx(
+        "absolute top-1/2 left-0 -translate-y-1/2 ml-[0.5rem] lg:ml-[2rem]",
+        isHome &&
+          shouldAnimate &&
+          "will-change-[top,left] animate-logo-enter-mobile lg:animate-logo-enter-lg"
+      )}
+    >
+      <div
+        className={clsx(
+          "overflow-hidden flex mt-7",
+          _theme === "dark" ? "flex-col-reverse" : "flex-col",
+          isHome &&
+            shouldAnimate &&
+            "animate-logo-overflow-mobile lg:animate-logo-overflow-lg will-change-[transform,width]"
+        )}
+      >
+        <IconComponent
+          key="logoLight"
+          name="logoLight"
+          width="138px"
+          className={clsx(
+            "duration-300",
+            isHome &&
+              shouldAnimate &&
+              "animate-main-logo-width-mobile lg:animate-main-logo-width-lg will-change-[width]",
+            _theme !== "light" && "opacity-0",
+            temperatureTheme === "winter" ? "fill-winter" : "fill-summer"
+          )}
+        />
+        <IconComponent
+          key="logoDark"
+          name="logoDark"
+          width="138px"
+          className={clsx(
+            "duration-300",
+            isHome &&
+              shouldAnimate &&
+              "animate-main-logo-width-mobile lg:animate-main-logo-width-lg will-change-[width]",
+            _theme !== "dark" && "opacity-0",
+            temperatureTheme === "winter" ? "fill-winter" : "fill-summer"
+          )}
+        />
+      </div>
+    </div>
+  );
 }
