51fbc8712bc0940877655acef7ada8f54c30c7cf
Daniil <boiko.81@list.ru>
2023-06-21 15:58:17 +0300

    feat: add animation for rocket flight

--- src/widgets/landing/hero/rocket.component.tsx
+++ src/widgets/landing/hero/rocket.component.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { IconComponent } from "@shared/ui";
 import clsx from "clsx";
 import { useAnimationStore } from "@shared/lib/store";
@@ -7,12 +7,25 @@ const iconClassnameByTheme =
   "first:dark:[&>*]:stroke-icon-on-primary first:[&>path]:stroke-icon-accent";
 const RocketComponent = () => {
   const { shouldAnimate } = useAnimationStore();
+  const [launch, setLaunch] = useState(false);
+
+  function rocketLaunch() {
+    if (launch) return;
+
+    setLaunch(true);
+
+    setTimeout(() => {
+      setLaunch(false);
+    }, 10000);
+  }
 
   return (
     <button
+      onClick={rocketLaunch}
       className={clsx(
         "absolute bottom-[1.5rem] left-[10.5rem] w-10 2xl:w-12 p-2 t-xs:bottom-2 t-xs:left-6",
-        shouldAnimate && "animate-hero-icons-fade-in"
+        shouldAnimate && "animate-hero-icons-fade-in",
+        launch && "lg:animate-rocket-flight-lg"
       )}
     >
       <IconComponent name="rocket" className={iconClassnameByTheme} />
