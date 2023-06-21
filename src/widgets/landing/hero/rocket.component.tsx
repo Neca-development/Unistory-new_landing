9cb291e284da7592a463e85b6d77e0297ee6bcac
Daniil <boiko.81@list.ru>
2023-06-21 16:43:50 +0300

    feat: connect and add GSAP for rocket animation

--- src/widgets/landing/hero/rocket.component.tsx
+++ src/widgets/landing/hero/rocket.component.tsx
@@ -1,35 +1,37 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import { IconComponent } from "@shared/ui";
 import clsx from "clsx";
 import { useAnimationStore } from "@shared/lib/store";
+import gsap from "gsap";
 
 const iconClassnameByTheme =
   "first:dark:[&>*]:stroke-icon-on-primary first:[&>path]:stroke-icon-accent";
 const RocketComponent = () => {
   const { shouldAnimate } = useAnimationStore();
-  const [launch, setLaunch] = useState(false);
+  // const [launch, setLaunch] = useState(false);
+  const rocketScope = useRef();
 
   function rocketLaunch() {
-    if (launch) return;
+    let ctx = gsap.context(() => {
+      gsap.to(".rocket", { rotation: 360 });
+    }, rocketScope);
 
-    setLaunch(true);
-
-    setTimeout(() => {
-      setLaunch(false);
-    }, 10000);
+    return () => ctx.revert();
   }
 
   return (
-    <button
-      onClick={rocketLaunch}
-      className={clsx(
-        "rocket absolute bottom-[1.5rem] left-[10.5rem] w-10 2xl:w-12 p-2 t-xs:bottom-2 t-xs:left-6",
-        shouldAnimate && "animate-hero-icons-fade-in"
-        // launch && "lg:animate-rocket-flight-lg"
-      )}
-    >
-      <IconComponent name="rocket" className={iconClassnameByTheme} />
-    </button>
+    <div ref={rocketScope}>
+      <button
+        onClick={rocketLaunch}
+        className={clsx(
+          "rocket absolute bottom-[1.5rem] left-[10.5rem] w-10 2xl:w-12 p-2 t-xs:bottom-2 t-xs:left-6",
+          shouldAnimate && "animate-hero-icons-fade-in"
+          // launch && "lg:animate-rocket-flight-lg"
+        )}
+      >
+        <IconComponent name="rocket" className={iconClassnameByTheme} />
+      </button>
+    </div>
   );
 };
 
