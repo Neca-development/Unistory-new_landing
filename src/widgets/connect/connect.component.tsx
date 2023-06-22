e7dffece91d1a8af4632dda71b967466de845654
Daniil <boiko.81@list.ru>
2023-06-22 14:26:05 +0300

    feat: disable sticky for "Ready to discuss..."

--- src/widgets/connect/connect.component.tsx
+++ src/widgets/connect/connect.component.tsx
@@ -13,7 +13,7 @@ export function Connect() {
   return (
     <section className="bg-dark-surface" id="become-customer">
       <div className="container pt-16 pb-[5.5rem] grid grid-cols-[1fr_38.75rem] gap-[10rem] text-dark-text-primary t-xs:grid-cols-1 t-xs:gap-0 t-xs:pb-12 relative">
-        <div className="md:sticky md:top-40 md:mt-7 h-fit">
+        <div className="md:mt-7 h-fit">
           <h2 className="font-bold text-5xl leading-[3.25rem] t-xs:text-2xl">{data.slogan}</h2>
           <p className="mt-9 text-2xl leading-10 t-xs:text-base">
             {data.description.beforeEmail}{" "}
