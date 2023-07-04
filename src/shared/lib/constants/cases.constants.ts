840233a8b04ff269f0f4489b8a3792d9a1b57adb
Daniil <boiko.81@list.ru>
2023-07-04 14:14:14 +0300

    feat: update texts for AdvancedRD case

--- src/shared/lib/constants/cases.constants.ts
+++ src/shared/lib/constants/cases.constants.ts
@@ -361,8 +361,8 @@ export const CASES: ICase[] = [
       ru: "Advanced RD",
     },
     categories: {
-      en: ["NFT-Marketplace"],
-      ru: ["NFT-Маркетплейс"],
+      en: ["Blockchain powered property investment platform"],
+      ru: ["Платформа для инвестиций в недвижимость на базе блокчейна"],
     },
     showCategoriesOnPage: false,
     icons: ["monitor", "phone02"],
@@ -382,8 +382,8 @@ export const CASES: ICase[] = [
       mob: advancedRDBanner.src,
     },
     description: {
-      en: "To create an NFT-marketplace for famous London Art Gallery where real artists with physical arts can sell their work as NFTs. Implement auction functionality and fixed price payments.",
-      ru: "Разработать NFT-Маркетплейс для знаменитой Лондонской Галереи, где художники смогу продавать свои физические работы как NFT. Интегрировать функционал аукциона и фиксированные платежи",
+      en: "To create an advanced property investment platform with full-fledged blockchain integration. You don’t need to pay the whole price by your own -- just share it with others and receive the dividents.  ",
+      ru: "Создать продвинутую платформу для инвестиций в недвижимость, с полноценной блокчейн-интеграцией. Вам не нужно платить всю сумму самому -- просто поделите её с другими и получайте дивиденды.",
     },
     params: {
       budget: {
@@ -396,14 +396,14 @@ export const CASES: ICase[] = [
     main: [
       {
         title: {
-          en: "Auction creation",
-          ru: "Создание аукциона",
+          en: "Property page",
+          ru: "Информация о недвижимости",
         },
         text: [
           {
             text: {
-              en: "Approved artists can start an auction and everyone can bid to buy a masterpiece as an NFT.",
-              ru: "Подтвержденные художники могут начать аукцион и каждый человек может сделать ставку на покупку шедевра искусства как NFT",
+              en: "Get all necessary information from the property page. Check the description, property price and amount of investments and make a decision to be a part of it. All information are powered by blockchain integration and updates in real time.",
+              ru: "Получайте всю необходимую информацию на одной странице. Прочитайте описание, посмотрите на цену и объем текущих инвестиций и решите -- стать ли частью этого. Вся информация обрабатывается на блокчейне и обрабатывается в режиме реального времени.",
             },
           },
         ],
@@ -411,14 +411,14 @@ export const CASES: ICase[] = [
       },
       {
         title: {
-          en: "All artists have a choise",
-          ru: "У каждого художника есть выбор",
+          en: "Wallet management",
+          ru: "Информация о недвижимости",
         },
         text: [
           {
             text: {
-              en: "It doesn’t have the «Highest Bid — Wins» pattern. The artist always can choose who’ll be the winner, no matter what’s the bid. Also, the artist doesn’t pay for mint — all expenses are up to buyer.",
-              ru: "Здесь нет принципа «Самая высокая ставка - выигрывает». Независимо от того, какой была последняя ставка: художник всегда может выбрать, кто будет победителем.",
+              en: "Track and manage your income on the wallet page. Ready to deposit or want to withdraw? You can make it happens in the one place.  Use the power of blockchain to proceed with all kind of payments and track your activity and income.",
+              ru: "Следите за своим заработком и управляйте им на странице кошелька. Готовы внести депозит или хотите вывести средства? Всё это можно сделать в одном месте. Используйте силу блокчейна чтобы управлять всеми видами платежей и следить за своей активностью и заработком.",
             },
           },
         ],
@@ -426,14 +426,14 @@ export const CASES: ICase[] = [
       },
       {
         title: {
-          en: "Real-time actions",
-          ru: "Real-time actions",
+          en: "Advanced referral system",
+          ru: "Продвинутая реферальная система",
         },
         text: [
           {
             text: {
-              en: "Easy-to-use dashboards to track the auction’s status with all necessary information.",
-              ru: "Easy-to-use dashboards to track the auction’s status with all necessary information.",
+              en: "We’ve implemented a great referral system to help you keep in touch wih your partners. Invite them to be in an investment community.  Here we have a two ways bonus system -- both of you will receive an extra income. And again, all is crystal clear and transparent since it’s managed by blockchain.",
+              ru: "Мы внедрили классную реферальную систему, чтобы вы могли держать связь со своими партнерами. Приглашайте их стать частью коммьюнити инвесторов и оба получайте бонусы -- система работает в обе стороны. И еще раз, все предельно прозрачно и понятно -- всё управляется блокчейном. ",
             },
           },
         ],
@@ -447,8 +447,8 @@ export const CASES: ICase[] = [
         text: [
           {
             text: {
-              en: "The platform is as friendly as possible, both for the user and for the author. It is built on a high-end architecture that guarantees scalability and trouble-free operation under load.",
-              ru: "Максимально дружелюбная, как для пользователя, так и для автора платформа. Построена на high-end архитектуре, гарантирующей масштабируемость и бесперебойную работу под нагрузкой. ",
+              en: "We’ve made a comprehensive blockchain integration to make investments transparent and easy.  We took over the whole process from smart contract to stablecoin creation. Besides, we’ve developed an admin panel for staff -- to manage the platform from A to Z. ",
+              ru: "Мы сделали обширную блокчейн-интеграцию, чтобы сделать инвестиции легким и прозрачным процессом. Мы взяли на себя весь функционал, от смарт контрактов до создания стейблкоина. Кроме того, мы разработали админ панель для сотрудников -- чтобы управлять платформой от А до Я.",
             },
           },
         ],
