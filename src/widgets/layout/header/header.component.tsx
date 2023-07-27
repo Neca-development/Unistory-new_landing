ea6378fddafa1bcf0a5ac1f509d2e31dfeada843
magtm <uporoff.maxim@yandex.ru>
2023-07-27 13:55:50 +0600

    fix header

--- src/widgets/layout/header/header.component.tsx
+++ src/widgets/layout/header/header.component.tsx
@@ -1,107 +1,108 @@
-import { ROUTESEN, ROUTESRU } from "@shared/i18n/constants";
-import clsx from "clsx";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import * as React from "react";
-import { Logo } from "./logo.component";
-import { MobileMenu } from "./mobile-menu";
-import { MenuBtn } from "./menu-btn.component";
-import { useEffect, useMemo, useState } from "react";
-import { useScrollDirection } from "@shared/lib/hooks/useScrollDirection.hook";
-import { useTheme } from "next-themes";
-import { useAnimationStore } from "@shared/lib/store";
-import useScrollbarSize from "react-scrollbar-size";
+import { ROUTESEN, ROUTESRU } from '@shared/i18n/constants'
+import { useScrollDirection } from '@shared/lib/hooks/useScrollDirection.hook'
+import { useAnimationStore } from '@shared/lib/store'
+import clsx from 'clsx'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useTheme } from 'next-themes'
+import * as React from 'react'
+import { useEffect, useMemo, useState } from 'react'
+import useScrollbarSize from 'react-scrollbar-size'
 
-export interface IHeaderProperties extends React.ComponentProps<"header"> {}
+import { Logo } from './logo.component'
+import { MenuBtn } from './menu-btn.component'
+import { MobileMenu } from './mobile-menu'
+
+export interface IHeaderProperties extends React.ComponentProps<'header'> {}
 
 export const Header = React.memo((props: IHeaderProperties) => {
-  const { className, children } = props;
+  const { className, children } = props
   // const router = useRouter()\
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-  const [isMenuDown, setIsMenuDown] = useState(false);
-  const [isLoad, setIsLoad] = useState(false);
-  const { theme, systemTheme } = useTheme();
-  const _theme = theme === "system" ? systemTheme : theme;
-  const { shouldAnimate } = useAnimationStore();
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false)
+  const [isMenuDown, setIsMenuDown] = useState(false)
+  const [isLoad, setIsLoad] = useState(false)
+  const { theme, systemTheme } = useTheme()
+  const _theme = theme === 'system' ? systemTheme : theme
+  const { shouldAnimate } = useAnimationStore()
 
-  const { locale } = useRouter();
-  const scrollDir = useScrollDirection();
-  const { width } = useScrollbarSize();
+  const { locale } = useRouter()
+  const scrollDir = useScrollDirection()
+  const { width } = useScrollbarSize()
 
-  const { asPath } = useRouter();
-  const currentPage = asPath;
+  const { asPath } = useRouter()
+  const currentPage = asPath
 
-  const isHome = asPath === "/";
+  const isHome = asPath === '/'
 
   const isMenuVisible = useMemo(() => {
-    return scrollDir !== "down";
-  }, [scrollDir]);
+    return scrollDir !== 'down'
+  }, [scrollDir])
 
   const ROUTES = React.useMemo(() => {
-    if (locale === "ru") {
-      return ROUTESRU;
+    if (locale === 'ru') {
+      return ROUTESRU
     }
 
-    return ROUTESEN;
-  }, [locale]);
+    return ROUTESEN
+  }, [locale])
 
   function openMenu() {
-    setIsMenuOpen(true);
+    setIsMenuOpen(true)
   }
 
   function closeMenu() {
-    setIsMenuOpen(false);
+    setIsMenuOpen(false)
   }
 
   useEffect(() => {
-    const scrollFunc = () => {
+    const scrollFunction = () => {
       if (window.scrollY > 100) {
-        setIsMenuDown(true);
+        setIsMenuDown(true)
       } else {
-        setIsMenuDown(false);
+        setIsMenuDown(false)
       }
-    };
+    }
 
     const timeout = setTimeout(() => {
-      setIsLoad(true);
-    }, 300);
+      setIsLoad(true)
+    }, 300)
 
-    document.addEventListener("scroll", scrollFunc);
+    document.addEventListener('scroll', scrollFunction)
 
     return () => {
-      document.removeEventListener("scroll", scrollFunc);
-      clearTimeout(timeout);
-    };
-  }, []);
+      document.removeEventListener('scroll', scrollFunction)
+      clearTimeout(timeout)
+    }
+  }, [])
 
   return (
     <header
       {...props}
-      className={clsx("relative flex w-full justify-center", className)}
+      className={clsx('relative z-50 flex w-full justify-center', className)}
       style={{
         paddingLeft: shouldAnimate ? width : 0,
       }}
     >
       <div
         className={clsx(
-          "flex w-full justify-center fixed z-10 transition-all duration-300",
-          "w-full px-[0.5rem] md:max-w-[790px] lg:max-w-[80rem] lg:px-[3rem] 2xl:max-w-[90rem] mx-auto m-l:max-w-full",
-          isMenuVisible ? "top-4" : "top-[-6rem] shadow-navbar"
+          'fixed z-10 flex w-full justify-center transition-all duration-300',
+          'mx-auto w-full px-[0.5rem] md:max-w-[790px] lg:max-w-[80rem] lg:px-[3rem] 2xl:max-w-[90rem] m-l:max-w-full',
+          isMenuVisible ? 'top-4' : 'top-[-6rem] shadow-navbar'
         )}
       >
         <div
           className={clsx(
-            "w-full flex justify-between py-4 lg:px-[2rem] px-[0.5rem] transition-all duration-500 relative items-center",
+            'relative flex w-full items-center justify-between py-4 px-[0.5rem] transition-all duration-500 lg:px-[2rem]',
             isMenuDown &&
-              "t-xs:py-2 bg-light-bg-accent shadow-navbar dark:bg-dark-surface dark:border-[#2b2b2b]"
+              'bg-light-bg-accent shadow-navbar dark:border-[#2b2b2b] dark:bg-dark-surface t-xs:py-2'
           )}
         >
           <Link
-            href={"/"}
+            href={'/'}
             className={clsx(
-              "w-[8.625rem] h-[31px] lg:h-[36px] 2xl:h-[40px]",
-              !shouldAnimate && "relative",
-              !isHome && "relative"
+              'h-[31px] w-[8.625rem] lg:h-[36px] 2xl:h-[40px]',
+              !shouldAnimate && 'relative',
+              !isHome && 'relative'
             )}
           >
             <Logo />
@@ -109,8 +110,8 @@ export const Header = React.memo((props: IHeaderProperties) => {
 
           <div
             className={clsx(
-              "hidden lg:flex items-center space-x-10",
-              currentPage === "/" && shouldAnimate && "animate-header-links-opacity"
+              'hidden items-center space-x-10 lg:flex',
+              currentPage === '/' && shouldAnimate && 'animate-header-links-opacity'
             )}
             style={{
               marginRight: shouldAnimate ? width : 0,
@@ -121,19 +122,20 @@ export const Header = React.memo((props: IHeaderProperties) => {
                 <Link
                   key={index}
                   className={clsx(
-                    " dark:text-light-bg-accent/[.5] transition duration-300 relative lg:text-lg 2xl:text-xl",
-                    !isLoad && "after:opacity-0",
-                    _theme === "light" ? "after:bg-dark-bg" : "after:bg-light-bg",
-                    route === "#become-customer"
-                      ? "text-light-text-primary dark:text-light-bg-accent font-bold	hover:!text-[#ec5f3b]"
-                      : "text-light-text-primary/[.5] dark:text-light-bg-accent/[.5] after:content-[''] after:absolute after:-bottom-1 hover:after:animate-link-hover-on after:h-0.5 after:animate-link-hover-off",
-                    currentPage.replace(/\/+$/, "") == route && "after:animate-link-hover-on"
+                    ' relative transition duration-300 dark:text-light-bg-accent/[.5] lg:text-lg 2xl:text-xl',
+                    !isLoad && 'after:opacity-0',
+                    _theme === 'light' ? 'after:bg-dark-bg' : 'after:bg-light-bg',
+                    route === '#become-customer'
+                      ? 'font-bold text-light-text-primary hover:!text-[#ec5f3b]	dark:text-light-bg-accent'
+                      : "text-light-text-primary/[.5] after:absolute after:-bottom-1 after:h-0.5 after:animate-link-hover-off after:content-[''] hover:after:animate-link-hover-on dark:text-light-bg-accent/[.5]",
+                    currentPage.replace(/\/+$/, '') == route &&
+                      'after:animate-link-hover-on'
                   )}
                   href={route}
                 >
                   {label}
                 </Link>
-              );
+              )
             })}
           </div>
 
@@ -141,7 +143,7 @@ export const Header = React.memo((props: IHeaderProperties) => {
 
           <button
             onClick={openMenu}
-            className="lg:hidden animate-header-links-opacity"
+            className="animate-header-links-opacity lg:hidden"
             style={{
               marginRight: shouldAnimate ? width : 0,
             }}
@@ -153,9 +155,9 @@ export const Header = React.memo((props: IHeaderProperties) => {
         </div>
       </div>
     </header>
-  );
-});
+  )
+})
 
-Header.displayName = "Header";
+Header.displayName = 'Header'
 
-export default Header;
+export default Header
