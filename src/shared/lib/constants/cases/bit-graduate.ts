1d32e5ed3fbdfe7b1c13c94a4bbb7b3e209e583b
Andrey Paskarenko <63596280+dynamic1717@users.noreply.github.com>
2023-07-31 18:34:57 +0600

    Fix bg image link

--- src/shared/lib/constants/cases/bit-graduate.ts
+++ src/shared/lib/constants/cases/bit-graduate.ts
@@ -1,6 +1,5 @@
 import { ICase, CustomHeroes, CustomMainBanner } from "@shared/lib/types";
 import { TEAM } from "@shared/lib/constants/team";
-import bitGraduateImage2 from "@public/assets/images/cases/bit-graduate/document-storage.png";
 import bitGraduateImage1 from "@public/assets/images/cases/bit-graduate/document-verification.png";
 import bitGraduateImage3 from "@public/assets/images/cases/bit-graduate/institute-authority.png";
 import bitGraduateThumb from "@public/assets/images/cases/bit-graduate/thumb.png";
