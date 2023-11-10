b8ebc33b106e29bb23162a2eeef7ee0d9637402e
MironovErnest <erik1297e@gmail.com>
2023-11-10 08:53:26 +0100

    feat: show company name

--- src/pages/cases/[id]/ui/review.component.tsx
+++ src/pages/cases/[id]/ui/review.component.tsx
@@ -1,36 +1,36 @@
-import type { ICase } from '@shared/lib/types'
-import Image from 'next/image'
-import { useRouter } from 'next/router'
-import React from 'react'
+import type { ICase } from "@shared/lib/types";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import React from "react";
 
 interface ICaseReviewProperties {
-  data: ICase
+  data: ICase;
 }
 
 export function CaseReview({ data }: ICaseReviewProperties) {
-  const { locale } = useRouter()
+  const { locale } = useRouter();
 
-  const { review, logo, title } = data
+  const { review, logo, title } = data;
 
   return (
     <section className="container relative mt-20 pb-[7.5rem] t-xs:mt-16 t-xs:pb-16">
       <article className="max-w-[52.5rem]">
         <p className="text-3xl font-medium leading-[3rem] t-xs:text-xl">
-          {review?.text[locale || 'ru']}
+          {review?.text[locale || "ru"]}
         </p>
         <div className="mt-5 flex items-center t-xs:mt-10 t-xs:block">
           {review?.author.photo && (
             <Image
               className="mr-6 w-[5.5rem] rounded-full t-xs:w-16"
               src={review.author.photo}
-              alt={review.author.name[locale || 'ru'] || ''}
+              alt={review.author.name[locale || "ru"] || ""}
               width={100}
               height={100}
             />
           )}
           <p className="text-2xl t-xs:ml-0 t-xs:mt-3 t-xs:text-base">
-            <span className="font-bold">{review?.author.name[locale || 'ru']}</span>,{' '}
-            {review?.author.position[locale || 'ru']}
+            <span className="font-bold">{review?.author.name[locale || "ru"]}</span>,{" "}
+            {review?.author.position[locale || "ru"]} {review?.author.company}
           </p>
         </div>
       </article>
@@ -44,5 +44,5 @@ export function CaseReview({ data }: ICaseReviewProperties) {
         />
       )}
     </section>
-  )
+  );
 }
