a2c4b92c6ac3ef19f21641fefa0ccaeb386d614f
Daniil <boiko.81@list.ru>
2024-02-28 13:52:08 +0100

    feat: contacts page update

--- src/shared/i18n/contacts/en.text.ts
+++ src/shared/i18n/contacts/en.text.ts
@@ -7,4 +7,16 @@ export const ContactsTextEn = {
       whatsappLabel: "Whatsapp",
     },
   },
+  ADDRESSES: {
+    SPB: {
+      title: "Санкт-Петербург",
+      phone: "+7 987 654 3210",
+      address: "ул. Гатчинская, 16, 1-й эт.,Санкт-Петербург, Россия, 197198",
+    },
+    ALMATA: {
+      title: "Алматы",
+      phone: "+7 987 654 3210",
+      address: "​ул. Байзакова, 252, 3-й эт., Алматы, 050037",
+    },
+  },
 };
--- src/widgets/landing/contacts/addresses.component.tsx
+++ src/widgets/landing/contacts/addresses.component.tsx
@@ -1,3 +1,27 @@
+import { ContactsTextRu, ContactsTextEn } from "@shared/i18n/contacts";
+import { useRouter } from "next/router";
+import { useMemo } from "react";
+
 export const ContactsAddresses = () => {
-  return <div>адресы</div>;
+  const { locale } = useRouter();
+
+  const langData = useMemo(() => {
+    return locale === "ru" ? ContactsTextRu : ContactsTextEn;
+  }, [locale]);
+
+  return (
+    <div className="container py-20 space-y-10">
+      {locale === "ru" && (
+        <div className="flex">
+          <div>
+            <h2 className="text-3xl font-bold mb-4">{langData.ADDRESSES.SPB.title}</h2>
+            <div className="text-xl space-y-2 opacity-50 xl:w-2/3">
+              <div>{langData.ADDRESSES.SPB.phone}</div>
+              <address className="not-italic">{langData.ADDRESSES.SPB.address}</address>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
