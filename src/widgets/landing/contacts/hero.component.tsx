c7f93e9090e975da537fe7374931b09a053adb3f
Daniil <boiko.81@list.ru>
2024-02-28 16:33:53 +0100

    styles: remove pattern-bg on contacts page

--- src/widgets/landing/contacts/hero.component.tsx
+++ src/widgets/landing/contacts/hero.component.tsx
@@ -11,48 +11,46 @@ export const ContactsHero = () => {
   }, [locale]);
 
   return (
-    <div className="bg-[url('/assets/images/Pattern.png')] -mt-[4.5rem] pt-[4.5rem]">
-      <div className="container py-10 md:py-28">
-        <h1 className="text-4xl font-bold mb-10 md:mb-28 md:text-[5rem] md:leading-tight">
-          {langData.HERO.title}
-        </h1>
-        <div className="justify-between space-y-10 md:flex md:space-y-0">
-          <div>
-            <div className="flex items-center space-x-3 font-semibold mb-3 text-2xl">
-              <span>Email</span>
-              <IconComponent name="contactsMail" />
-            </div>
-            <a
-              href="mailto:asmolnikov@unistory.app"
-              className="text-2xl font-bold dark:text-light-bg-accent hover:opacity-50 transition-all md:text-4xl"
-            >
-              asmolnikov@unistory.app
-            </a>
+    <div className="container py-10 md:py-28">
+      <h1 className="text-4xl font-bold mb-10 md:mb-28 md:text-[5rem] md:leading-tight">
+        {langData.HERO.title}
+      </h1>
+      <div className="justify-between space-y-10 md:flex md:space-y-0">
+        <div>
+          <div className="flex items-center space-x-3 font-semibold mb-3 text-2xl">
+            <span>Email</span>
+            <IconComponent name="contactsMail" />
           </div>
-          <div>
-            <div className="flex items-center space-x-3 font-semibold mb-3 text-2xl">
-              <span>Telegram</span>
-              <IconComponent name="contactsTelegram" />
-            </div>
-            <a
-              href="https://t.me/Antonunistory"
-              className="text-2xl font-bold dark:text-light-bg-accent hover:opacity-50 transition-all md:text-4xl"
-            >
-              @Antonunistory
-            </a>
+          <a
+            href="mailto:asmolnikov@unistory.app"
+            className="text-2xl font-bold dark:text-light-bg-accent hover:opacity-50 transition-all md:text-4xl"
+          >
+            asmolnikov@unistory.app
+          </a>
+        </div>
+        <div>
+          <div className="flex items-center space-x-3 font-semibold mb-3 text-2xl">
+            <span>Telegram</span>
+            <IconComponent name="contactsTelegram" />
           </div>
-          <div>
-            <div className="flex items-center space-x-3 font-semibold mb-3 text-2xl">
-              <span>WhatsApp</span>
-              <IconComponent name="contactsWhatsApp" />
-            </div>
-            <a
-              href="https://wa.me/79874490825"
-              className="text-2xl font-bold dark:text-light-bg-accent hover:opacity-50 transition-all md:text-4xl"
-            >
-              +7 987 449-08-25
-            </a>
+          <a
+            href="https://t.me/Antonunistory"
+            className="text-2xl font-bold dark:text-light-bg-accent hover:opacity-50 transition-all md:text-4xl"
+          >
+            @Antonunistory
+          </a>
+        </div>
+        <div>
+          <div className="flex items-center space-x-3 font-semibold mb-3 text-2xl">
+            <span>WhatsApp</span>
+            <IconComponent name="contactsWhatsApp" />
           </div>
+          <a
+            href="https://wa.me/79874490825"
+            className="text-2xl font-bold dark:text-light-bg-accent hover:opacity-50 transition-all md:text-4xl"
+          >
+            +7 987 449-08-25
+          </a>
         </div>
       </div>
     </div>
