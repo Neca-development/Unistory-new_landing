022aef6027e3b8710d85432af4f7644236727896
Daniil <boiko.81@list.ru>
2024-02-29 12:14:12 +0100

    test: test bug solution with maps

--- src/widgets/landing/contacts/addresses.component.tsx
+++ src/widgets/landing/contacts/addresses.component.tsx
@@ -13,51 +13,47 @@ export const ContactsAddresses = () => {
   }, [locale]);
 
   return (
-    <div className="container bg-light-bg-accent dark:bg-dark-bg py-10 md:py-20">
-      <div className={clsx("justify-between mb-10 md:mb-20 md:flex", locale === "en" && "!hidden")}>
-        <div className="mb-10 md:mb-0 md:w-1/3">
-          <h2 className="text-3xl font-bold mb-4">{langData.ADDRESSES.SPB.title}</h2>
-          <div className="text-xl space-y-2 opacity-50 xl:w-4/5">
-            <div>{langData.ADDRESSES.SPB.phone}</div>
-            <address
-              className="not-italic"
-              dangerouslySetInnerHTML={{ __html: langData.ADDRESSES.SPB.address }}
-            />
+    <YMaps query={{ lang: locale === "ru" ? "ru_RU" : "en_US" }}>
+      <div className="container bg-light-bg-accent dark:bg-dark-bg py-10 md:py-20">
+        {locale === "ru" && (
+          <div className={clsx("justify-between mb-10 md:mb-20 md:flex")}>
+            <div className="mb-10 md:mb-0 md:w-1/3">
+              <h2 className="text-3xl font-bold mb-4">{langData.ADDRESSES.SPB.title}</h2>
+              <div className="text-xl space-y-2 opacity-50 xl:w-4/5">
+                <div>{langData.ADDRESSES.SPB.phone}</div>
+                <address
+                  className="not-italic"
+                  dangerouslySetInnerHTML={{ __html: langData.ADDRESSES.SPB.address }}
+                />
+              </div>
+            </div>
+            <div className="h-72 md:w-3/5 md:h-96">
+              <Map width="100%" height="100%" state={{ center: [59.96, 30.324], zoom: 16 }}>
+                <Placemark
+                  geometry={[59.96, 30.324]}
+                  options={{
+                    iconLayout: "default#image",
+                    iconImageSize: [50, 62],
+                    iconImageHref: placemarkIcon.src,
+                  }}
+                />
+              </Map>
+            </div>
           </div>
-        </div>
-        <div className="h-72 md:w-3/5 md:h-96">
-          <YMaps>
-            <Map width="100%" height="100%" defaultState={{ center: [59.96, 30.324], zoom: 16 }}>
-              <Placemark
-                geometry={[59.96, 30.324]}
-                options={{
-                  iconLayout: "default#image",
-                  iconImageSize: [50, 62],
-                  iconImageHref: placemarkIcon.src,
-                }}
+        )}
+        <div className="justify-between md:flex">
+          <div className="mb-10 md:mb-0 md:w-1/3">
+            <h2 className="text-3xl font-bold mb-4">{langData.ADDRESSES.ALMATA.title}</h2>
+            <div className="text-xl space-y-2 opacity-50 xl:w-4/5">
+              <div>{langData.ADDRESSES.ALMATA.phone}</div>
+              <address
+                className="not-italic"
+                dangerouslySetInnerHTML={{ __html: langData.ADDRESSES.ALMATA.address }}
               />
-            </Map>
-          </YMaps>
-        </div>
-      </div>
-      <div className="justify-between md:flex">
-        <div className="mb-10 md:mb-0 md:w-1/3">
-          <h2 className="text-3xl font-bold mb-4">{langData.ADDRESSES.ALMATA.title}</h2>
-          <div className="text-xl space-y-2 opacity-50 xl:w-4/5">
-            <div>{langData.ADDRESSES.ALMATA.phone}</div>
-            <address
-              className="not-italic"
-              dangerouslySetInnerHTML={{ __html: langData.ADDRESSES.ALMATA.address }}
-            />
+            </div>
           </div>
-        </div>
-        <div className="h-72 md:w-3/5 md:h-96">
-          <YMaps query={{ lang: locale === "ru" ? "ru_RU" : "en_US" }}>
-            <Map
-              width="100%"
-              height="100%"
-              defaultState={{ center: [43.241209, 76.91494], zoom: 16 }}
-            >
+          <div className="h-72 md:w-3/5 md:h-96">
+            <Map width="100%" height="100%" state={{ center: [43.241209, 76.91494], zoom: 16 }}>
               <Placemark
                 geometry={[43.241209, 76.91494]}
                 options={{
@@ -67,9 +63,9 @@ export const ContactsAddresses = () => {
                 }}
               />
             </Map>
-          </YMaps>
+          </div>
         </div>
       </div>
-    </div>
+    </YMaps>
   );
 };
