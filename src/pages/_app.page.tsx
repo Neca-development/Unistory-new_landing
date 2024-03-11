3ef29311065b465cb91872d80e99162c57c15bb6
Daniil <boiko.81@list.ru>
2024-03-11 16:18:18 +0100

    feat: add hotjar metric

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,11 +2,13 @@ import "@shared/styles/global.scss";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import TagManager from "react-gtm-module";
+import { hotjar } from "react-hotjar";
 import { useEffect } from "react";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
     TagManager.initialize({ gtmId: "G-Q3N3QQXJV3" });
+    hotjar.initialize(3900084, 6);
   }, []);
   return (
     <ThemeProvider attribute="class">
