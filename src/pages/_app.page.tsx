c9a83510ce9cb100971e2d8b3d75b7692524c2c0
AndreyBS1 <andrey.babenkov.00@gmail.com>
2024-07-18 18:25:47 +0300

    fix: use next 3-party library support from GTM

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,19 +1,19 @@
 import "@shared/styles/global.scss";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
-import TagManager from "react-gtm-module";
 import { hotjar } from "react-hotjar";
 import { useEffect } from "react";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
-    TagManager.initialize({ gtmId: "G-Q3N3QQXJV3" });
     hotjar.initialize(3900084, 6);
   }, []);
   return (
     <ThemeProvider attribute="class">
       {/* <ThemeContextProvider> */}
       <Component {...pageProps} />
+      <GoogleTagManager gtmId="G-Q3N3QQXJV3" />
       <div id="portal" />
       {/* </ThemeContextProvider> */}
     </ThemeProvider>
