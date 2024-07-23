317a37808deb5b2019ee44dce3b2896f52aa9ba3
AndreyBS1 <andrey.babenkov.00@gmail.com>
2024-07-23 17:21:57 +0300

    fix: place analytics scripts at the top of html body

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,7 +3,6 @@ import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import { hotjar } from "react-hotjar";
 import { useEffect } from "react";
-import { GoogleTagManager } from "@next/third-parties/google";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
@@ -13,7 +12,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <ThemeProvider attribute="class">
       {/* <ThemeContextProvider> */}
       <Component {...pageProps} />
-      <GoogleTagManager gtmId="G-Q3N3QQXJV3" />
       <div id="portal" />
       {/* </ThemeContextProvider> */}
     </ThemeProvider>
