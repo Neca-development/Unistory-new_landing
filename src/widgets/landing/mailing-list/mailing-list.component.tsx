ec01f4773ac7aca6c9cd64160dc8e7ca1ab2f32e
unistoryMaksimAnokhin <manokhin@unistory.app>
2024-09-26 15:47:06 +0300

    fixed design

--- src/widgets/landing/mailing-list/mailing-list.component.tsx
+++ src/widgets/landing/mailing-list/mailing-list.component.tsx
@@ -49,13 +49,20 @@ export const MailingList = () => {
   };
 
   return (
-    <div className="rounded-lg mb-[8.75rem] bg-mailing-list-bg-light dark:bg-mailing-list-bg mx-[4.125rem] p-[3.375rem] mt-[5.25rem] m-md:mx-[1.125rem] m-md:px-4 m-md:py-7">
-      <div className="text-[2.875rem] font-bold m-md:text-[1.75rem]">
-        <p>{locale === "ru" ? "Подпишитесь на рассылку" : "Subscribe to the newsletter"}</p>
+    <div className="rounded-xl mb-[8.75rem] bg-mailing-list-bg-light dark:bg-mailing-list-bg mx-[5rem] p-[3.375rem] mt-[5.25rem] m-md:mx-[1.125rem] m-md:px-4 m-md:py-7">
+      <div className="text-[2.875rem] font-bold m-md:text-[1.75rem] leading-[3.375rem] m-md:leading-[2rem]">
+        {locale === "ru" ? (
+          <p>Подпишитесь на рассылку</p>
+        ) : (
+          <>
+            <p>Subscribe</p>
+            <p>to the newsletter</p>
+          </>
+        )}
         <p>{locale === "ru" ? "с AI-инсайдами" : "with AI insights"}</p>
       </div>
       <div className="mt-[6.25rem] flex justify-between m-md:block m-md:mt-5">
-        <div className="text-lg dark:text-dark-text-secondary w-[22rem] m-md:w-full m-md:text-sm">
+        <div className="text-lg dark:text-dark-text-secondary w-[25.0625rem] leading-[1.375rem] m-md:w-full m-md:text-sm">
           <p>{locale === "ru" ? "Только раз в месяц." : "Only once a month."}</p>
           <p>
             {locale === "ru"
@@ -75,7 +82,7 @@ export const MailingList = () => {
               type="text"
               name="email"
               placeholder={locale === "ru" ? "Эл. почта" : "Email"}
-              className="w-[20.5625rem] border-b h-full dark:border-bg-secondary bg-transparent mr-6 m-md:w-full"
+              className="w-[20.5625rem] dark:placeholder:text-[white] h-full border-b-2 outline-none rounded-none pt-6 pb-3 leading-7 dark:border-bg-secondary bg-transparent mr-6 m-md:w-full"
             />
             {errors.email && <p className="text-error">{errors.email.message}</p>}
             {error && <p className="text-error">{error}</p>}
@@ -89,9 +96,9 @@ export const MailingList = () => {
           </div>
           <button
             type="submit"
-            className="px-[5.375rem] bg-bg-accent text-[1.5rem]leading-6 font-semibold m-md:mt-6 m-md:h-[3.625rem] m-md:w-full text-[white]"
+            className="px-[5.375rem] bg-bg-accent rounded-[0.125rem] text-[1.5rem]leading-6 font-semibold m-md:mt-6 m-md:h-[3.625rem] m-md:w-full text-[white]"
           >
-            Подписаться
+            {locale === "ru" ? "Подписаться" : "Subscribe"}
           </button>
         </form>
       </div>
--- src/widgets/landing/news/news.component.tsx
+++ src/widgets/landing/news/news.component.tsx
@@ -7,11 +7,26 @@ export const News = () => {
   const { locale } = useRouter();
 
   return (
-    <div className="mt-28 mx-[5.875rem] m-md:mx-[1.125rem]">
+    <div className="px-[5rem] m-md:mx-[1.125rem]  m-md:p-0">
       <div className="flex justify-between items-end m-md:block">
-        <div className="text-[2.875rem] font-bold m-md:text-[1.75rem]">
-          <p>{locale === "ru" ? "Рассказываем про работу в IT" : "We talk about working in IT,"}</p>
-          <p>{locale === "ru" ? "нейросети и web3" : "neural networks and web3"}</p>
+        <div className="text-[2.875rem] font-bold m-md:text-[1.75rem] leading-[3.375rem] m-md:leading-[2rem]">
+          {locale === "ru" ? (
+            <>
+              <p>Рассказываем про работу в IT</p>
+              <p>нейросети и web3</p>
+            </>
+          ) : (
+            <>
+              <div className="m-md:hidden">
+                <p>We talk about working in IT,</p>
+                <p>neural networks and web3</p>
+              </div>
+              <div className="md:hidden">
+                <p>We talk about</p>
+                <p>working in IT, neural networks and web3</p>
+              </div>
+            </>
+          )}
         </div>
         <button className="rounded-full p-2 bg-light-bg-accent dark:bg-dark-bg-secondary flex items-center max-h-[3.75rem] m-md:max-h-10 m-md:mt-6">
           <span className="ml-9 mr-6 text-lg font-semibold m-md:text-sm m-md:ml-5">
