bc3f58b0064e99f8591817284429bf268bc3ed23
VladislavPH <vpuzyrev@unistory.app>
2024-11-05 11:10:10 +0300

    generate-book update

--- src/widgets/landing/generate-book/generated-book-checkbox/checkbox.component.tsx
+++ src/widgets/landing/generate-book/generated-book-checkbox/checkbox.component.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./checkbox.module.scss";
 
 interface BookCheckboxProps {
   isChecked: boolean;
@@ -7,12 +8,10 @@ interface BookCheckboxProps {
 
 const BookCheckbox: React.FC<BookCheckboxProps> = ({ isChecked, onToggle }) => {
   return (
-    <input
-      type="checkbox"
-      checked={isChecked}
-      onClick={onToggle}
-      className="absolute w-5 h-5 bg-light-bg rounded-xl top-1 right-1 pointer-events-none"
-    />
+    <label className="wrapper">
+      <input type="checkbox" checked={isChecked} onChange={onToggle} className="checkbox" />
+      <span className="checkmark"></span>
+    </label>
   );
 };
 
